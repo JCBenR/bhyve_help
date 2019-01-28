77acbd6b987b1569f49df254e78ce82cb14355bc
JCBenR <jsieb81@gmail.com>
2019-01-28 12:52:56 -0700

    got the send working

--- public/JS/quillapp.js
+++ public/JS/quillapp.js
@@ -15,21 +15,44 @@ var quill = new Quill('#editor', {
     theme: 'snow'
 });
 
+const getPosts = () => {
+    return fetch(`https://jsonplaceholder.typicode.com/posts`)
+    .then(res => res.json())
+    .then(posts => console.log(posts))
+}
+
+
 
 function test1ab (){
     var bugs = document.querySelectorAll('input[type=checkbox]:checked');
     var bugs2a = {};
+    var quesB;
+    var ansB;
     bugs.forEach((item) => {
         bugs2a[item.name] = item.value
     });
-    var data = {
-        method: 'POST',
-        data: {"test": "hello world"},
-        headers: {
-            "Content-Type": "application/json"
+    quesB = document.getElementById('question1').value;
+    ansB = quill.root.innerHTML;
+    const post = {
+        categories: bugs2a,
+        title: quesB,
+        body: ansB, 
+        userId: 3
+    }
+    
+    const newPost = post => {
+        const options = {
+            method: "POST",
+            body: JSON.stringify(post),
+            headers: new Headers({
+                'Content-Type': 'application/json'
+            })
         }
+        return fetch(`https://jsonplaceholder.typicode.com/posts`, options)
+            .then(res => res.json())
+            .then(res => console.log(res))
+            .catch(error => console.error(`Error: ${error}`))
     };
-    fetch('/help/new', data)
-
-    return (JSON.stringify(bugs2a));
+    
+    newPost(post);
 };
